28061a7960d8f526a265b6bed263ad82833680e7
fe-olaf <frontend.olaf@gmail.com>
2021-08-02 18:12:13 +0900

    type 예제 추가

---
+++ javascript/type.js
@@ -0,0 +1,36 @@
+// 0. JS 동적 타입 언어 
+var a = ''
+
+typeof a // string
+
+// 1. 원시 타입 
+const num = 10 // number
+const s = 'string' // string
+const u = undefined // undefined
+const n = null // null
+const b = true // boolean
+const sl = new Symbol() // symbol
+
+// 2. 객체 타입 
+const arr = []
+const obj = {}
+
+// typescript 가 왜 필요한가 ? 
+// add 라는 함수는 숫자를 받는다. 더한다.
+function add(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('인자 값은 모두 숫자여야합니다.')
+    }
+
+    return a + b
+}
+
+add(10, 20) // 30
+add('짱', '구') // 짱구
+
+// function add2(a: number, b: number): number {
+//     return a + b
+// }
+
+add2(10, 20) // OK !
+add2('짱') // Error
